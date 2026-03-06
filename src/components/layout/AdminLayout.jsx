9e697eb37bde083a3eb0bf163c01b35c5678d701
PrathameshBhavsar-Dev <prathamesh69bhavsar@gmail.com>
2026-03-06 15:00:59 +0530

    made changes in the admin navbar

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -15,8 +15,8 @@ const AdminLayout = () => {
         </div>
 
         {/* Content */}
-        <div className="flex-1 flex flex-col">
-          <AdminNavbar />
+        <div className="m-4 flex-1 flex flex-col">
+          {/* <AdminNavbar /> */}
           <div className="flex-1 p-6 bg-gray-100 overflow-y-auto">
             <Outlet />
           </div>
