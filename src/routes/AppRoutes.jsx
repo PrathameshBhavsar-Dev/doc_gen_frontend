a245357e9e2ec84ed146a0182ae56631ab4eb8e1
Tarushi <dusanetarushi2020@gmail.com>
2026-03-07 11:15:28 +0530

    constant,approutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -23,6 +23,8 @@ const AdminCompanyManagementPage = lazy(
 );
 const AdminSettingPage = lazy(() => import("../pages/admin/AdminSettingPage"));
 const AdminAddCompany = lazy(() => import("../components/admin/companyManagement/AddCompany"));
+const AdminCompanyDetails = lazy(() => import("../components/admin/companyManagement/CompanyDetails"));
+
 
 /* ======================= USER ======================= */
 const UserDashboardPage = lazy(() => import("../pages/user/UserDashboardPage"));
@@ -105,6 +107,10 @@ const AppRoutes = () => {
           <Route
             path={ROUTES.ADMIN_ADD_COMPANY}
             element={<LazyLoad component={AdminAddCompany} />}
+          />
+          <Route
+            path={ROUTES.ADMIN_COMPANY_DETAILS}
+            element={<LazyLoad component={AdminCompanyDetails} />}
           />
         </Route>
       </Route>
