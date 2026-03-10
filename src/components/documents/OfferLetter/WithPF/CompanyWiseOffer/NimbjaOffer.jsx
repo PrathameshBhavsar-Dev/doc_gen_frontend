b32200a1ef5da5f9f352bb1a7f6aa95832f24775
Saidas2020 <morvekarsai@gmail.com>
2026-03-10 12:47:39 +0530

    Changes are Updated

--- src/components/documents/OfferLetter/WithPF/CompanyWiseOffer/NimbjaOffer.jsx
+++ src/components/documents/OfferLetter/WithPF/CompanyWiseOffer/NimbjaOffer.jsx
@@ -132,7 +132,7 @@ const NimbjaOffer = ({ company, data }) => {
 
             {/* SUBJECT */}
             <Typography sx={{ mb: 2, fontFamily: "Bahnschrift", mt: "-2mm" }}>
-              Subject : Letter of intent for the position of {data.designation}
+              Subject : Letter of intent for the position of {data.position}
             </Typography>
 
             <Typography sx={{ mb: 3, fontFamily: "Bahnschrift" }}>
